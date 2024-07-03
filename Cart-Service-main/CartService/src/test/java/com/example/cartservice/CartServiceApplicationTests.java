a68750f236d4baf9e7b10e7c99a180f0a9601933
hyliam23 <102239117+hyliam23@users.noreply.github.com>
2024-07-03 23:48:13 +0800

    Add files via upload

---
+++ Cart-Service-main/CartService/src/test/java/com/example/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.cartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartServiceApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(CartServiceApplicationTests.class, args);
+	}
+}
